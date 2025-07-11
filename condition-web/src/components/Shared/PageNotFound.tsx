2887059ed2feb48aba035ec0ec1fa2878eb0c6c8
VineetBala-AOT <vineet.balachandran@aot-technologies.com>
2025-07-11 11:13:08 -0700

    Improving page not found and deployment

--- condition-web/src/components/Shared/PageNotFound.tsx
+++ condition-web/src/components/Shared/PageNotFound.tsx
@@ -1,38 +1,65 @@
-import { Grid, Typography, Toolbar } from '@mui/material';
-import ErrorSvg from '../../../src/assets/images/404.svg';
-import { Link } from "@tanstack/react-router";
+import { Toolbar, Grid, Typography, Box, Link } from "@mui/material";
+import ErrorSvg from "@/assets/images/404.svg";
+import { AppConfig } from "@/utils/config";
 
-const marginStyle = { mr: 2 };
-
-const PageNotFound = ({ errorMessage }: { errorMessage?: string }) => {
-
-    return (
-        <>
-            <Toolbar />
-            <Grid
-                container
-                direction={'column'}
-                justifyContent="center"
-                alignItems="center"
-                spacing={1}
-                padding={'2em 2em 1em 2em'}
-            >
-                <Grid item sx={{ ...marginStyle, marginBottom: 3 }}>
-                    <Typography variant="h1">
-                      {errorMessage}
-                    </Typography>
-                </Grid>
-                <Grid item sx={{ marginStyle, marginBottom: 2 }}>
-                <img src={ErrorSvg} alt="404 Not Found" />
-                </Grid>
-                <Grid item xs={6} justifyContent={'left'}>
-                  <Typography variant="h4">
-                    <Link to={"/"}>Go Home</Link>
-                  </Typography>
-                </Grid>
-            </Grid>
-        </>
-    );
-};
-
-export default PageNotFound;
+export default function PageNotFound() {
+  return (
+    <>
+      <Toolbar />
+      <Grid
+        container
+        direction={"column"}
+        justifyContent="center"
+        alignItems="center"
+        spacing={1}
+        padding={"2em 2em 1em 2em"}
+      >
+        <Grid item sx={{ marginBottom: 3 }}>
+          <Typography variant="h2">
+            The page you are looking for cannot be found.
+          </Typography>
+        </Grid>
+        <Grid item sx={{ marginBottom: 2 }}>
+          <img
+            src={ErrorSvg}
+            alt="Page not found"
+            style={{
+              width: "35em",
+              height: "20em",
+              boxSizing: "border-box",
+              padding: "0px",
+            }}
+          />
+        </Grid>
+        <Grid item xs={6} justifyContent="center" mb={4}>
+          <Typography variant="h2">
+            The page you are looking for might have been moved or is temporarily
+            unavailable.
+          </Typography>
+        </Grid>
+        <Grid item xs={6} justifyContent={"left"}>
+          <Box sx={{ mt: 1 }}>
+            <Typography variant="h5" style={{ fontWeight: "bold" }}>
+              Suggestions to help you find what you're looking for:
+            </Typography>
+            <ul>
+              <li>{"Check that the web URL has been entered correctly."}</li>
+              <li>
+                {`Go to our `}
+                <Link href="/">homepage</Link>
+                {` and browse through our past and current submissions.`}
+              </li>
+              <li>{`Telephone Device for the Deaf (TDD) across B.C.: 711`}</li>
+              <li>
+                {`If you would like to email us please contact `}
+                <Link href={`mailto:${AppConfig.supportEmail}`}>
+                  {AppConfig.supportEmail}
+                </Link>
+              </li>
+            </ul>
+          </Box>
+        </Grid>
+      </Grid>
+    </>
+  );
+}
