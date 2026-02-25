266e7e952b75fecd3b2de07b4facebe8f4eca1e7
VineetBala-AOT <vineet.balachandran@aot-technologies.com>
2026-02-24 16:36:20 -0800

    handle confilct error message

--- condition-web/src/hooks/api/useConditions.tsx
+++ condition-web/src/hooks/api/useConditions.tsx
@@ -9,6 +9,7 @@ import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { Options } from "./types";
 import { notify } from "@/components/Shared/Snackbar/snackbarStore";
 import { defaultUseQueryOptions, QUERY_KEY } from "./constants";
+import { HTTP_STATUS_CODES } from "../../hooks/api/constants";
 
 const fetchConditions = (includeSubconditions: boolean, projectId?: string, documentId?: string) => {
   if (!projectId) {
@@ -127,7 +128,14 @@ export const useCreateCondition = (
       });
       notify.success("Condition created successfully")
     },
-    onError: () => notify.error("Failed to create condition"),
+    onError: (error: { response?: { data?: { message?: string }; status?: number } }) => {
+      if (error.response?.status === HTTP_STATUS_CODES.CONFLICT) {
+        notify.error(error.response?.data?.message || "Failed to create condition");
+      }
+      else {
+        notify.error("Failed to create condition")
+      }
+    },
     ...options,
   });
 };
